ed50e6dac3d3c52db537b1866ba1390ae21b4a6f
josores2 <josesorliquart@gmail.com>
2024-04-04 19:38:30 +0200

    Initial Commit

---
+++ para subir al git/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  para subir al git
+//
+//  Created by Jose on 4/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ para subir al git/para_subir_al_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  para_subir_al_gitApp.swift
+//  para subir al git
+//
+//  Created by Jose on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct para_subir_al_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
